4a7b58dc04b2518f406a619b11241acb683a306a
Tamer Durgun <tmr.durgun@gmail.com>
2022-02-16 20:19:52 +0300

    add new todo

--- src/hooks/components/Todos.js
+++ src/hooks/components/Todos.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 
 const Todos = () => {
+  const [newTodo, setNewTodo] = useState('');
 
   const [todos, setTodos] = useState([
     {
@@ -13,7 +14,7 @@ const Todos = () => {
       "userId": 1,
       "id": 2,
       "title": "quis ut nam facilis et officia qui",
-      "completed": false
+      "completed": true
     },
     {
       "userId": 1,
@@ -21,18 +22,6 @@ const Todos = () => {
       "title": "fugiat veniam minus",
       "completed": false
     },
-    {
-      "userId": 1,
-      "id": 4,
-      "title": "et porro tempora",
-      "completed": true
-    },
-    {
-      "userId": 1,
-      "id": 5,
-      "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
-      "completed": false
-    }
   ]);
 
   const handleCheckTodo = (todoItem) => {
@@ -42,22 +31,41 @@ const Todos = () => {
     }))
   }
 
-  return (
-    <ul>
-      {todos.map((todoItem, i) => (
-        <li key={`todo-item-${i + 1}`} style={{ textDecoration: todoItem.completed ? 'line-through' : 'none' }}>
-          <label htmlFor={`todo-cb-${i + 1}`} style={{ cursor: 'pointer' }}>
-            {todoItem.title}
+  const handleAddTodo = () => {
+    setTodos([...todos, {
+      "userId": 1,
+      "id": todos.length + 1,
+      "title": newTodo,
+      "completed": false
+    }])
+  }
 
-            <input type="checkbox" name="" id={`todo-cb-${i + 1}`}
-              value={todoItem.completed}
-              checked={todoItem.completed}
-              onChange={() => handleCheckTodo(todoItem)}
-            />
-          </label>
-        </li>
-      ))}
-    </ul>
+  return (
+    <div>
+      <div className="add-todo-container flex">
+        <input type="text" value={newTodo}
+          placeholder='What else to do?'
+          onChange={(e) => setNewTodo(e.target.value)}
+        /> <button onClick={() => handleAddTodo()}>Add +</button>
+      </div>
+
+      <ul>
+        {todos.map((todoItem, i) => (
+          <li key={`todo-item-${i + 1}`} style={{ textDecoration: todoItem.completed ? 'line-through' : 'none' }}>
+            <label htmlFor={`todo-cb-${i + 1}`} style={{ cursor: 'pointer' }}>
+              {todoItem.title}
+
+              <input type="checkbox" name="" id={`todo-cb-${i + 1}`}
+                value={todoItem.completed}
+                checked={todoItem.completed}
+                onChange={() => handleCheckTodo(todoItem)}
+              />
+            </label>
+          </li>
+        ))}
+      </ul>
+    </div>
+
   )
 }
 
